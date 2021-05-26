7eb1bb2a5427ce517ca7398fac1eac82ad106076
swathikotegar <84147127+swathikotegar@users.noreply.github.com>
2021-05-26 23:02:30 +0530

    code to print number of 0 and 1 pattern
    you will get the triangled pattern of 0 and 1 in 6 rows

---
+++ task4.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+main()
+{
+int n,i,j;
+printf("enter the number of rows:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+	for(j=1;j<=i;j++)
+	{
+		printf("01");
+	
+	}
+		printf("\n");
+}
+	//printf("1\n");
+
+	return 0;
+}
